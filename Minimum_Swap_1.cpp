f08a128f16223a1bba78551320403a0e2d1b59a4
kashishbhandula <56387644+kashishbhandula@users.noreply.github.com>
2021-10-04 12:17:47 +0530

    Create Minimum_Swap_1.cpp

---
+++ Minimum_Swap_1.cpp
@@ -0,0 +1,149 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define in long long int
+#define ll int
+#define ld long double
+#define dd double
+#define repf(i, j, k) for (in i = j; i < k; i++)
+#define repb(i, j, k) for (in i = j; i > k; i--)
+#define repff(i, j, k) for (in i = j; i <= k; i++)
+#define repbb(i, j, k) for (in i = j; i >= k; i--)
+#define MOD 10000007
+#define mod 10000007
+#define endl "\n"
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//debug template starts
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '"' << x << '"'; }
+void __print(const string &x) { cerr << '"' << x << '"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+typedef tree<in, null_type, less_equal<in>, rb_tree_tag, tree_order_statistics_node_update> i_set; //indexed_set
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINEJUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+//debug template ends
+
+#define YES()                  \
+    ;                          \
+    {                          \
+        cout << "YES" << endl; \
+        return;                \
+    }
+#define Yes()                  \
+    ;                          \
+    {                          \
+        cout << "Yes" << endl; \
+        return;                \
+    }
+#define NO()                  \
+    ;                         \
+    {                         \
+        cout << "NO" << endl; \
+        return;               \
+    }
+#define No()                  \
+    ;                         \
+    {                         \
+        cout << "No" << endl; \
+        return;               \
+    }
+in tim = 0;
+void google(in n)
+{
+    cout << "Case #" << tim << ":"
+         << " " << n << endl;
+    return;
+}
+in Minimum_Swap(vector<in> &a, vector<in> &b)
+{
+    in cnt = 0;
+    in n=a.size();
+    vector<vector<in>> hash(1000007);
+
+    repf(i, 0, n)
+    {
+        hash[a[i]].push_back(i);
+    }
+    repf(i, 0, n)
+    {
+        if (a[i] != b[i])
+        {
+            cnt++;
+            in idx = *hash[b[i]].begin();
+            hash[b[i]].erase(hash[b[i]].begin());
+            hash[a[i]].erase(hash[a[i]].begin());
+            hash[a[i]].push_back(idx);
+            swap(a[i], a[idx]);
+            
+        }
+    }
+    return cnt;
+}
+void solve()
+{
+    in n;
+    cin >> n;
+    vector<in> a(n);
+    vector<in> b;
+    repf(i, 0, n) cin >> a[i];
+    debug(a);
+    
+    b = a;
+    sort(b.begin(), b.end());
+    
+    debug(b);
+   
+     cout<<Minimum_Swap(a, b)<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    
+}
