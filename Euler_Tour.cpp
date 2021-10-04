f33c2e6edc9abef008090ad491dad8b9725ceafc
kashishbhandula <56387644+kashishbhandula@users.noreply.github.com>
2021-10-04 16:51:02 +0530

    Create Euler_Tour.cpp

---
+++ Euler_Tour.cpp
@@ -0,0 +1,146 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define in long long int
+#define ll int
+#define ld long double
+#define dd double
+#define repf(i, j, k) for (in i = j; i < k; i++)
+#define repb(i, j, k) for (in i = j; i > k; i--)
+#define repff(i, j, k) for (in i = j; i <= k; i++)
+#define repbb(i, j, k) for (in i = j; i >= k; i--)
+#define MOD 10000007
+#define mod 10000007
+#define endl "\n"
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//debug template starts
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '"' << x << '"'; }
+void __print(const string &x) { cerr << '"' << x << '"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+typedef tree<in, null_type, less_equal<in>, rb_tree_tag, tree_order_statistics_node_update> i_set; //indexed_set
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINEJUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+//debug template ends
+
+#define YES()                  \
+    ;                          \
+    {                          \
+        cout << "YES" << endl; \
+    }
+#define Yes()                  \
+    ;                          \
+    {                          \
+        cout << "Yes" << endl; \
+        return;                \
+    }
+#define NO()                  \
+    ;                         \
+    {                         \
+        cout << "NO" << endl; \
+    }
+#define No()                  \
+    ;                         \
+    {                         \
+        cout << "No" << endl; \
+        return;               \
+    }
+in tim = 0;
+void google(in n)
+{
+    cout << "Case #" << tim << ":"
+         << " " << n << endl;
+    return;
+}
+void Recursion(in node, vector<vector<in>> &graph, vector<in> &ht, vector<in> &vis, vector<in> &subtree,vector<in>&Euler, in parent = 0)
+{
+    vis[node] = 0;
+    ht[node] = 1 + ht[parent];
+    Euler.push_back(node);
+    
+    for (auto it : graph[node])
+    {
+        if (vis[it])
+        {
+            Recursion(it, graph, ht, vis, subtree,Euler,node);
+            Euler.push_back(node);
+        }
+        if (parent != it)
+        {
+            subtree[node] += subtree[it];
+            
+        }
+    }
+}
+void solve()
+{
+    in n;
+    cin >> n;
+    vector<vector<in>> graph(n + 1);
+    repf(i, 0, n-1)
+    {
+        in u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    vector<in> ht(n + 1);
+    vector<in> subtree(n + 1, 1);
+    vector<in> vis(n + 1, 1);
+    vector<in> Euler;
+    Recursion(1, graph, ht, vis, subtree,Euler);
+    debug(Euler);
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+}
